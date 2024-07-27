0d98151cd0ff95e0c69fce7e1d3733c96c255f5c
Konstantin <chachabooms@gmail.com>
2024-07-28 02:10:42 +0300

    добавлены метрики

--- my_line_reg.py
+++ my_line_reg.py
@@ -1,10 +1,13 @@
 import numpy as np
 
 class MyLineReg():
-    def __init__(self, n_iter, learning_rate):
+    def __init__(self, n_iter, learning_rate, metric = ''):
         self.n_iter = n_iter
         self.learning_rate = learning_rate
         self.weights = []
+        self.metric = metric
+        self.result_metric = 0
+
     def fit(self, X, y, verbose = 0):
         X = X.to_numpy()
         y = y.to_numpy()
@@ -16,16 +19,54 @@ class MyLineReg():
         for x in range(0,self.n_iter):
             y_pred = np.dot(X, self.weights)
             loss = y_pred - y
+            norm = y - np.mean(y_pred)
             m = np.size(y)
             cost = np.sum(loss ** 2) / m
+            
             grad = 2 * np.dot(X.T, loss) / m
             self.weights = self.weights - self.learning_rate * grad
+            
+            if(self.metric == 'mae'):
+                self.result_metric = np.sum(abs(loss)) / m
+            elif(self.metric == 'mse'):
+                self.result_metric = cost
+            elif(self.metric == 'rmse'):
+                self.result_metric = cost ** 0.5
+            elif(self.metric == 'mape'):
+                self.result_metric = np.sum(abs(loss / y)) * (100 / m)
+            elif(self.metric == 'r2'):
+                self.result_metric = 1 - np.sum(loss ** 2) / np.sum(norm ** 2)
+                
             if(verbose != 0):
-                print(f"start | loss: {cost}")
+                if self.metric == '':
+                    print(f"start | loss: {cost}")
+                else:
+                    print(f"start | loss: {cost} | {self.metric}: {self.result_metric}")
+                
                 if x == log_param:
-                    print(f"{x} | loss: {cost}")
+                    if self.metric == '':
+                        print(f"{x} | loss: {cost}")
+                    else:
+                        print(f"{x} | loss: {cost} | {self.metric}: {self.result_metric}")
                     log_param = log_param + verbose
-                    
+        
+        y_pred = np.dot(X, self.weights)
+        loss = y_pred - y
+        norm = y - np.mean(y_pred)
+        m = np.size(y)
+        
+        if(self.metric == 'mae'):
+            self.result_metric = np.sum(abs(loss)) / m
+        elif(self.metric == 'mse'):
+            self.result_metric = cost
+        elif(self.metric == 'rmse'):
+            self.result_metric = cost ** 0.5
+        elif(self.metric == 'mape'):
+            self.result_metric = np.sum(abs(loss / y)) * (100 / m)
+        elif(self.metric == 'r2'):
+            self.result_metric = 1 - np.sum(loss ** 2) / np.sum(norm ** 2)
+        
+
     def predict(self, X):
         X = np.hstack([np.ones((X.shape[0],1)), X])
         y_pred = np.dot(X, self.weights)
@@ -33,6 +74,9 @@ class MyLineReg():
     
     def get_coef(self):
         return self.weights[1:]
+    
+    def get_best_score(self):
+        return self.result_metric
 
     def __str__(self):  
         return f"MyLineReg class: n_iter={self.n_iter}, learning_rate={self.learning_rate}"
