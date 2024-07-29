35c9609db74e277ddc74c8cf726c85be045af24d
Konstantin <chachabooms@gmail.com>
2024-07-29 23:31:20 +0300

    добавил метод get_metrics

--- my_line_reg.py
+++ my_line_reg.py
@@ -7,6 +7,18 @@ class MyLineReg():
         self.weights = []
         self.metric = metric
         self.result_metric = 0
+        
+    def __get_metrics(self, y, loss, norm, cost, m):
+        if(self.metric == 'mae'):
+            self.result_metric = np.sum(abs(loss)) / m
+        elif(self.metric == 'mse'):
+            self.result_metric = cost
+        elif(self.metric == 'rmse'):
+            self.result_metric = cost ** 0.5
+        elif(self.metric == 'mape'):
+            self.result_metric = np.sum(abs(loss / y)) * (100 / m)
+        elif(self.metric == 'r2'):
+            self.result_metric = 1 - np.sum(loss ** 2) / np.sum(norm ** 2)
 
     def fit(self, X, y, verbose = 0):
         X = X.to_numpy()
@@ -16,27 +28,21 @@ class MyLineReg():
         self.weights = np.ones(X.shape[1])
         
         log_param = verbose
-        for x in range(0,self.n_iter):
+        for x in range(0,self.n_iter + 1):
             y_pred = np.dot(X, self.weights)
-            loss = y_pred - y
-            norm = y - np.mean(y_pred)
+            loss = y_pred - y 
+            norm = y - np.mean(y)
             m = np.size(y)
             cost = np.sum(loss ** 2) / m
+            self.__get_metrics(y, loss, norm, cost, m)
+            
+            
+            if(x == self.n_iter + 1): 
+                break
             
             grad = 2 * np.dot(X.T, loss) / m
             self.weights = self.weights - self.learning_rate * grad
             
-            if(self.metric == 'mae'):
-                self.result_metric = np.sum(abs(loss)) / m
-            elif(self.metric == 'mse'):
-                self.result_metric = cost
-            elif(self.metric == 'rmse'):
-                self.result_metric = cost ** 0.5
-            elif(self.metric == 'mape'):
-                self.result_metric = np.sum(abs(loss / y)) * (100 / m)
-            elif(self.metric == 'r2'):
-                self.result_metric = 1 - np.sum(loss ** 2) / np.sum(norm ** 2)
-                
             if(verbose != 0):
                 if self.metric == '':
                     print(f"start | loss: {cost}")
@@ -49,23 +55,6 @@ class MyLineReg():
                     else:
                         print(f"{x} | loss: {cost} | {self.metric}: {self.result_metric}")
                     log_param = log_param + verbose
-        
-        y_pred = np.dot(X, self.weights)
-        loss = y_pred - y
-        norm = y - np.mean(y_pred)
-        m = np.size(y)
-        
-        if(self.metric == 'mae'):
-            self.result_metric = np.sum(abs(loss)) / m
-        elif(self.metric == 'mse'):
-            self.result_metric = cost
-        elif(self.metric == 'rmse'):
-            self.result_metric = cost ** 0.5
-        elif(self.metric == 'mape'):
-            self.result_metric = np.sum(abs(loss / y)) * (100 / m)
-        elif(self.metric == 'r2'):
-            self.result_metric = 1 - np.sum(loss ** 2) / np.sum(norm ** 2)
-        
 
     def predict(self, X):
         X = np.hstack([np.ones((X.shape[0],1)), X])
