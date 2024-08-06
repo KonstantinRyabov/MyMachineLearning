ee1cea7505977868c747a27a2dba295bdc509e08
Konstantin <chachabooms@gmail.com>
2024-08-07 00:29:59 +0300

    метод ближайших соседей

---
+++ my_KNNClf.py
@@ -0,0 +1,14 @@
+class MyKNNClf():
+    def __init__(self, k = 0, train_size = ()):
+        self.k = k
+        self.train_size = train_size
+        self.X = []
+        self.y = []
+        
+    def fit(self, X, y):
+        self.X = X.to_numpy()
+        self.y = y.to_numpy()
+        self.train_size = X.shape
+    
+    def __str__(self):  
+        return f"MyKNNClf class: k={self.k}"
