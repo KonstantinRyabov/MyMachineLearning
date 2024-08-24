66a680e75029859067a92e225e6a4234dab50349
Konstantin <chachabooms@gmail.com>
2024-08-24 22:32:15 +0300

    доделал алгоримт kmeans

--- my_MyKMeans.py
+++ my_MyKMeans.py
@@ -6,46 +6,50 @@ class MyKMeans():
         self.max_iter = max_iter
         self.n_init = n_init
         self.random_state = random_state
+        self.cluster_centers_ = []
+        self.inertia_ = 0
     
     def __str__(self):  
         return f"MyKMeans class: n_clusters={self.n_clusters}, max_iter={self.max_iter}, n_init={self.n_init}, random_state={self.random_state}"
+    def __closest_centroid(self, points, centroids):
+        distances = np.sqrt(((points - centroids[:, np.newaxis])**2).sum(axis=2))
+        return np.argmin(distances, axis=0)
     
-    def __calc_dis(self, X, centroid):
-        distance = np.sqrt(np.sum(np.power(X - centroid, 2), axis=1))
-        return distance
-        
+    def __distance_centroid(self, points, centroids):
+        distances = np.sqrt(((points - centroids[np.newaxis, :])**2).sum(axis=1))
+        return distances
+
     def fit(self, X):
         np.random.seed(self.random_state)
         X = X.to_numpy()
-        n, m = X.shape
+        _, m = X.shape
         centroids = np.empty((self.n_clusters, m))
         min = np.min(X, axis=0)
         max = np.max(X, axis=0)
         wcss_arr = np.empty((self.n_init))
         centroids_arr = np.empty((self.n_init, self.n_clusters, m))
-        for i in range(self.n_init):
+        for n_i in range(self.n_init):
+            # инициализация кластеров
             for item in range(self.n_clusters):
                 centroids[item] = np.random.uniform(min, max)
+            # ищем ближайший кластера и двигаем их
             for _ in range(self.max_iter):
-                distanсes = np.empty((self.n_clusters, n))
-                for i in range(self.n_clusters):
-                    centroid_sample = np.full((n, m), centroids[i])
-                    distanсes[i] = self.__calc_dis(X, centroid_sample)
-                indx = np.argmin(distanсes)
-
-                centroids_temp = np.empty((self.n_clusters, m))
-                wcss = 0
-                for item in range(self.n_clusters):
-                    cluster = X[np.where(indx == item)]
-                    n_cl,m_cl = cluster.shape
-                    centroids_temp[item] = np.mean(cluster, axis = 0)
-                    centroid_sample = np.full((n_cl, m_cl), centroids_temp[item])
-                    dis_sum = np.sum(np.power(self.__calc_dis(cluster, centroid_sample), 2), axis = 0)
-                    wcss = wcss + dis_sum
-                wcss_arr[i] = wcss
-                if np.array_equal(centroids, centroids_temp):
-                    centroids = centroids_temp
-                    break
-                centroids = centroids_temp
-                
-                centroids_arr[i] = centroids
+                closest_indx = self.__closest_centroid(X, centroids)
+                for k in range(self.n_clusters):
+                    cluster = X[closest_indx == k]
+                    centroids[k] = np.mean(cluster, axis=0) if np.any(cluster) else centroids[k]
+            
+            # ищем внутрикластерные расстояния
+            closest_indx = self.__closest_centroid(X, centroids)
+            wcss = 0
+            for k in range(self.n_clusters):
+                cluster = X[closest_indx == k]
+                dis_centr = np.sum((self.__distance_centroid(cluster, centroids[k])**2), axis = 0)
+                wcss += dis_centr
+            wcss_arr[n_i] = wcss
+            centroids_arr[n_i] = centroids
+        
+        # берем минимальное wcss
+        min_index = np.argmin(wcss_arr)
+        self.inertia_ = wcss_arr[min_index]
+        self.cluster_centers_ = centroids_arr[min_index]
